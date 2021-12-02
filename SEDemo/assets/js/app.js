2d1c8ea97e1aee86496ba6edb89ddb7c65013e60
Crystal Atoz <catoz@nevada.unr.edu>
2021-12-01 16:47:07 -0800

    Added Progress Bar

--- SEDemo/assets/js/app.js
+++ SEDemo/assets/js/app.js
@@ -111,3 +111,24 @@
     });
   }
 /* END SECTION FROM GITHUB TEMPLATE FOR NAVBAR FUNCTIONALITY */
+
+// Progress Bar 
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
+  }
+} 
