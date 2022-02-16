eb67d50c5df8c630524dc0f47d523dd16c90f727
atozc <catoz@nevada.unr.edu>
2022-02-16 15:34:35 -0800

    Updated course templates

---
+++ src/views/DataScienceCourse.vue
@@ -0,0 +1,36 @@
+<!-- ====== TEMPLATE ====== -->
+<template>
+    <custom-header title="My Courses"></custom-header>
+</template>
+
+<!-- ======= SCRIPT ======= -->
+<script>
+import CustomHeader from '../components/Header.vue'
+
+export default{
+    components:
+    {
+        CustomHeader,
+    },
+    setup() {
+        
+    },
+}
+</script>
+
+<!-- ======= STYLE ======= -->
+<style scoped>
+.card {
+	width: 34rem;
+}
+
+.parent {
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	flex-wrap: wrap;
+	width: 100%;
+	flex: 1;
+}
+
+</style>
