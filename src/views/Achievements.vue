7a0b748b96106a214416d3fb6a65d1b4f6de9ec1
atozc <catoz@nevada.unr.edu>
2022-03-23 15:03:15 -0700

    Moved badge info to new vue page

--- src/views/Achievements.vue
+++ src/views/Achievements.vue
@@ -1,8 +1,7 @@
-<!-- popup achievement system (HTML/CSS/JS) - adapted from codepen.io -->
-
 <!-- ======= TEMPLATE ======= -->
 <template>
     <custom-header title="Achievements"></custom-header>
+    
     <div class="container mt-4">
         <progress-bar :progress="(unlocked.length / (locked.length + unlocked.length)) * 100" />
 
@@ -10,10 +9,10 @@
             <div v-for="(achievement, index) in achievements" v-bind:key="index">
                 <card :horizontal="true" :image="achievement.image" :class="['mt-4', achievement.unlocked ? '' : 'locked']"> 
                     <template #title>
-                        {{achievement.AchievementTitle}}
+                        {{achievement.title}}
                     </template>
                     <template #body>
-                        {{achievement.AchievementDescription}}
+                        {{achievement.description}}
                     </template>
                 </card> 
             </div>
@@ -23,58 +22,22 @@
 
 <!-- ======= SCRIPT ======= -->
 <script>
-import { computed, onBeforeMount } from 'vue';
+import { computed } from 'vue';
 import CustomHeader from '@/components/Header.vue'
 import Card from '@/components/Card.vue'
 import ProgressBar from '@/components/ProgressBar.vue';
-//import { ref, onBeforeMount } from "vue";
-import axios from 'axios';
-
-const achievementsDB = 'http://localhost:3001/api/achievements'
-//const userDB = 'http://localhost:3001/api/userpost'
-
 export default {
-    components:
+  components:
     {
         CustomHeader,
         Card,
-        ProgressBar
+        ProgressBar,
     },
     setup() {
-
-        //get user id
-        //const id = localStorage.getItem('ID'); 
-
-        //get achievement
-        //check achievement requirements
-        //award achievement & update user profile - event triggered  w/ pop-up of badge icon
-
-        //const showAward = ref(false) //setup for popup badge
-        //Achievement Data to insert to database 
-        const AchievementTitle = ref('')
-        const AchievementDescription = ref('')
-        //const AchievementRequirements = ref('')
-        //const giveBadge = ref('')
-        //const removeBadge = ref('')
-        const BadgeID = ref('')
-
-        // access achievement database for display 
-        onBeforeMount(async () => {
-            await axios.get(achievementsDB)
-                .then(response => {
-                    achievements.value = response.data;
-
-                }).catch(err => {
-                    console.error(err);
-                });
-            },
-        );
-
         const achievements = [
             {
-                BadgeID: 1,
-                AchievementTitle: "Welcome to the Class",
-                AchievementDescription: "Earned when you first registered.",
+                title: "Welcome to the Class",
+                description: "Earned when you first registered.",
                 image: {
                     src: "img/badges/Cup_Badge_Color.png",
                     alt: "Welcome to the Class",
@@ -82,9 +45,8 @@ export default {
                 unlocked: true,
             },
             {
-                BadgeID: 2,
-                AchievementTitle: "E-Learning Newbie!",
-                AchievementDescription: "Earned upon completing your first lesson.",
+                title: "E-Learning Newbie!",
+                description: "Earned upon completing your first lesson.",
                 image: {
                     src: "img/badges/Growth_Badge_Color.png",
                     alt: "E-Learning Newbie",
@@ -92,9 +54,8 @@ export default {
                 unlocked: false,
             },
             {
-                BadgeID: 3,
-                AchievementTitle: "Brainiac in the Making!",
-                AchievementDescription: "Earned when you ace your first quiz.",
+                title: "Brainiac in the Making!",
+                description: "Earned when you ace your first quiz.",
                 image: {
                     src: "img/badges/Idea_Badge_Color.png",
                     alt: "Brainiac in the Making",
@@ -102,9 +63,8 @@ export default {
                 unlocked: false,
             },
             {
-                BadgeID: 4,
-                AchievementTitle: "Baby Steps!",
-                AchievementDescription: "Earned when you complete you complete your first topic.",
+                title: "Baby Steps!",
+                description: "Earned when you complete you complete your first topic.",
                 image: {
                     src: "img/badges/Like_Badge_Color.png",
                     alt: "Baby Steps",
@@ -112,9 +72,8 @@ export default {
                 unlocked: false,
             },
             {
-              BadgeID: 5,
-              AchievementTitle: "Knowledge Knight",
-              AchievementDescription: "Earned when you complete your first course.",
+              title: "Knowledge Knight",
+              description: "Earned when you complete your first course.",
               image: {
                   src: "img/badges/Medal_Badge_Color.png",
                   alt: "Knowledge Knight",
@@ -122,9 +81,8 @@ export default {
               unlocked: false,
             },
           {
-            BadgeID: 6,
-            AchievementTitle: "On a Roll!",
-            AchievementDescription: "Earned when you complete 10 lessons.",
+            title: "On a Roll!",
+            description: "Earned when you complete 10 lessons.",
             image: {
                 src: "img/badges/Monitor_Badge_Color.png",
                 alt: "On a Roll",
@@ -132,9 +90,8 @@ export default {
             unlocked: false,
           },
           {
-            BadgeID: 7,
-            AchievementTitle: "First Streak!",
-            AchievementDescription: "Earned when you learn for 3 days straight.",
+            title: "First Streak!",
+            description: "Earned when you learn for 3 days straight.",
             image: {
               src: "img/badges/Shuttle_Badge_Color.png",
               alt: "First Streak",
@@ -142,9 +99,8 @@ export default {
             unlocked: false,
           },
           {
-            BadgeID: 8,
-            AchievementTitle: "Overachiever!",
-            AchievementDescription: "Earned when you complete all courses available.",
+            title: "Overachiever!",
+            description: "Earned when you complete all courses available.",
             image: {
               src: "img/badges/Star_Badge_Color.png",
               alt: "Overachiever",
@@ -152,47 +108,14 @@ export default {
             unlocked: false,
           }
         ];
-
-
         const unlocked = computed(() => {
             return achievements.filter(achievement => achievement.unlocked);
         });
-
         const locked = computed(() => {
             return achievements.filter(achievement => !achievement.unlocked);
         });
-
-        // Insert data into achievement table
-	    async function postBadge(){
-
-		    const submitObject = {
-			    AchievementTitle: this.AchievementTitle,
-			    AchievementDescription: this.AchievementDescription,
-		    }
-
-		    // Front End error handling goes here
-		
-		    await axios.post(AchievementsDB, submitObject)
-			    .then((res) => {
-				    submitObject.BadgeID = res.data.insertId
-				    achievements.value.push(submitObject)
-			    }).catch(err => {
-				    console.error(err)
-			    })
-	    }
-
         return {
-            achievements, 
-            unlocked, 
-            locked,
-            AchievementTitle,
-            AchievementDescription,
-            //AchievementRequirements,
-            //showAward,
-            //giveBadge,
-            //removeBadge,
-            BadgeID,
-            postBadge
+            achievements, unlocked, locked,
         }
     },
 }
@@ -203,7 +126,6 @@ export default {
     .card {
         width: 25rem;
     }
-
     .parent {
         display: flex;
         justify-content: space-around;
@@ -211,5 +133,4 @@ export default {
         flex-wrap: wrap;
         flex: 1 0 33%;
     }
-
 </style>
