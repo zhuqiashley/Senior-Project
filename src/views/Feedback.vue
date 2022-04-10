fa1c285c582ab1781d9748367521c8d2d2a3a972
Yan <yshore@nevada.unr.edu>
2022-04-10 00:00:36 -0700

    add Feedback for user course

---
+++ src/views/Feedback.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 style="margin-left: 100px;margin-top: 500px;">Feedback</h1>
+</template>
+
+<script>
+export default {
+  name: "Feedback",
+  props: ['title', 'id'],
+  mounted() {
+    console.log(this.title)
+    console.log(this.id)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
